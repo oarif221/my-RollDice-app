b0f24cc1e746f683e7fc036155eaf12a42e515e9
Omer Arif <oamsak@gmail.com>
2017-02-15 14:14:27 +0100

    Initial Commit

---
+++ RollDice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RollDice
+//
+//  Created by Omer Arif on 15/02/2017.
+//  Copyright © 2017 OmerA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
